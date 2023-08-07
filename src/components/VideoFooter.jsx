f9a1bf541be4d258a917ad605f7f6fbaf3619f47
eyobed101 <eyocjjazz101@gmail.com>
2023-08-07 15:21:27 +0300

    make the data dynamic

--- src/components/VideoFooter.jsx
+++ src/components/VideoFooter.jsx
@@ -1,32 +1,32 @@
 import React, { useEffect, useState, useRef } from "react";
 import "./VideoFooter.css";
 import MusicNoteIcon from "@material-ui/icons/MusicNote";
-import Tickers from "./Ticker";
+import Ticker from "react-ticker";
 
-const VideoFooter = () => {
-  const marqueeRef = useRef(null);
-
+const VideoFooter = ({ channel, description, song }) => {
   return (
     <div className="videoFooter">
       <div className="videoFooter__text">
-        <h3>@kebede</h3>
-        <p>Macbook Air to new Windows editing beast</p>
+        <h3>@{channel} </h3>
+        <p>{description}</p>
         <div className="videoFooter__ticker">
           <MusicNoteIcon className="videoFooter__icon" />
-          
+          <Ticker mode="smooth">
+            {({ index }) => (
+              <>
+                <p>{song}</p>
+              </>
+            )}
+          </Ticker>
         </div>
-        {/* <Tickers />  */}
       </div>
-     
       <img
         className="videoFooter__record"
-        src="https://static.thenounproject.com/png/934821-200.png"
+        src="https://static.
+    thenounproject.com/png/934821-200.png"
         alt="video footer"
       />
-   
     </div>
-    
   );
 };
-
 export default VideoFooter;
